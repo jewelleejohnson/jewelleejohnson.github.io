c7836e2de187c54a8b42f960bdf9abadd3192c96
jewelleejohnson <jewelleejohnson@gmail.com>
2026-02-18 21:58:26 -0500

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -1,13 +1,8 @@
-<nav>
-  <a href="index.html">Home</a>
-  <a href="assets.html">3D Assets</a>
-  <a href="unity.html">Unity</a>
-  <a href="av.html">Audiovisual</a>
-  <a href="resume.html">Resume</a>
-</nav>
+// SCENE
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x111111);
 
+// CAMERA
 const camera = new THREE.PerspectiveCamera(
   45,
   600 / 400,
@@ -16,25 +11,39 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(0, 1, 3);
 
+// RENDERER
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(600, 400);
 document.getElementById("viewer").appendChild(renderer.domElement);
 
-const light1 = new THREE.DirectionalLight(0xffffff, 1);
-light1.position.set(5, 5, 5);
-scene.add(light1);
+// LIGHTS
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 
-const light2 = new THREE.AmbientLight(0x404040);
-scene.add(light2);
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
 
+// CONTROLS
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// LOAD OBJ
 const loader = new THREE.OBJLoader();
-loader.load("models/sample.obj", function (object) {
-  scene.add(object);
-});
+loader.load(
+  "3d-assets/Jewel_CD.obj",
+  function (object) {
+    scene.add(object);
+  },
+  undefined,
+  function (error) {
+    console.error("OBJ failed to load", error);
+  }
+);
 
+// ANIMATE
 function animate() {
   requestAnimationFrame(animate);
-  scene.rotation.y += 0.003;
+  controls.update();
   renderer.render(scene, camera);
 }
 
